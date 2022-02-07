4d0ab3f71fd45f5877a1606c12bf6efaae6be1b2
jun <watanabejapan2trap@gmail.com>
2022-02-07 14:20:18 +0900

    p260まで作成

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -9,12 +9,14 @@ public class NejikoController : MonoBehaviour
 
     Vector3 moveDirection = Vector3.zero;
 
+    //charactorControllの時は、自分で重力を設定の必要がある。
     public float gravity;
     public float speedZ;
     public float speedJump;
     // Start is called before the first frame update
     void Start()
     {
+        //controller = this.gameObject.GetComponent<CharacterController>();
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
@@ -22,8 +24,10 @@ public class NejikoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //キャラクターが地面に接地してたら
         if (controller.isGrounded)
         {
+            //上を押すと+1、下を押すと-1
             if (Input.GetAxis("Vertical") > 0.0f)
             {
                 moveDirection.z = Input.GetAxis("Vertical") * speedZ;
@@ -32,23 +36,32 @@ public class NejikoController : MonoBehaviour
             {
                 moveDirection.z = 0;
             }
+            //キャラクターの回転
+            //unityは左手系の座標だから右を押すと時計回り
             transform.Rotate(0, Input.GetAxis("Horizontal") * 3, 0);
 
             if (Input.GetButton("Jump"))
             {
                 moveDirection.y = speedJump;
-                animator.SetTrigger("Jump");
+                animator.SetTrigger("jump");
             }
         }
 
+        //deltaTimeは、1フレームに要する時間。
+        //なぜ掛けるかというと、1秒間に６０フレームじゃない環境（例えば古いパソコン）があった場合でも
+        //動きが遅くならないようにする。
         moveDirection.y -= gravity * Time.deltaTime;
 
+        //TransformDirectionは、キャラクターが向いた方向のベクトル（つまりz方向）に変換
         Vector3 globalDirection = transform.TransformDirection(moveDirection);
         controller.Move(globalDirection * Time.deltaTime);
 
-        if (controller.isGrounded) moveDirection.y = 0;
-
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0;
+        }
         animator.SetBool("run", moveDirection.z > 0.0f);
-
+        //controllerは、キャラクターの動きの制御
+        //animatorはキャラクターの見た目の制御
     }
 }
---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    Vector3 diff;
+
+    public GameObject target;
+    public float followSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        diff = target.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //LateUpdateが処理される度に距離が縮まりその距離の割合で決まるので
+        //最初の移動距離は長く段々移動距離が短くなる。最初はダッシュ、止まる直前はピタっと止まるのではなく
+        //段々止まる感じになる。
+        transform.position = Vector3.Lerp(
+            //A地点
+            transform.position,
+            //B地点
+            target.transform.position - diff,
+            //A-B地点の間の割合
+            Time.deltaTime * followSpeed
+            );
+    }
+}
