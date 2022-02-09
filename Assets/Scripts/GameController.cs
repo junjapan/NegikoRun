e972e179432ba0d2b8e0a428760948f9c7136761
jun <watanabejapan2trap@gmail.com>
2022-02-09 14:05:12 +0900

    p311まで作成

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+//    public GameObject nejiko;
+    public NejikoController nejiko;
+    //    public Transform nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+    // Update is called once per frame
+    void Update()
+    {
+        int score = CalcScore();
+        scoreText.text = $"Score:{score}m";
+
+//        lifePanel.UpdateLife(nejiko.GetComponent<NejikoController>().Life());
+        lifePanel.UpdateLife(nejiko.Life());
+
+    }
+
+    int CalcScore()
+    {
+        return (int)nejiko.transform.position.z;
+    }
+}
