09c72f5ab5ac22525c6df8d9c2de926a6d4d0fe3
jun <watanabejapan2trap@gmail.com>
2022-02-14 11:01:09 +0900

    完成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -19,10 +20,27 @@ public class GameController : MonoBehaviour
 //        lifePanel.UpdateLife(nejiko.GetComponent<NejikoController>().Life());
         lifePanel.UpdateLife(nejiko.Life());
 
+        if (nejiko.Life() <= 0)
+        {
+            enabled = false;
+
+            if (PlayerPrefs.GetInt("HighScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+
+            Invoke("ReturnToTitle", 2.0f);
+        }
+
     }
 
     int CalcScore()
     {
         return (int)nejiko.transform.position.z;
+    }
+
+    void ReturnToTitle()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    public Text highScoreText;
+
+    public void Start()
+    {
+        highScoreText.text = "High Score:" + PlayerPrefs.GetInt("HighScore") + "m";
+    }
+    public void onStartButtonClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void onClearScoreButtonClicked()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+
+        highScoreText.text = "High Score:" + PlayerPrefs.GetInt("HighScore") + "m";
+    }
+}
